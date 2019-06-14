2a119f880d24b881613c39c91729f44313e145c4
NextMeIn <dishanshali@gmail.com>
2019-06-14 13:14:49 -0400

    A bug indentified. Final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,26 +11,26 @@ with open(election_data_csv, 'r', newline = '', encoding='utf-8') as csvfile:
         if x[2] not in data.keys():
             data[x[2]] = 1
         else:
-            data[x[2]] = data[x[2]] + 1
+            data[x[2]] += 1
 
 def totalvotes(dict1):
     total = 0
     for x, y in dict1.items():
-        total = total + y
+        total += y
     return total
 
 def dataprint(dict1):
     output = "Election Results\n--------------------------"
     total = totalvotes(dict1)
-    output = output + "\nTotal Votes: " + str(total) + "\n--------------------------"
+    output += "\nTotal Votes: " + str(total) + "\n--------------------------"
     winner = ''
     winnervotes = 0
     for x, y in dict1.items():
-        output = output + "\n" + x + ": " + str(round((y/total)*100,2)) + "% (" + str(y) + ")"
+        output += "\n" + x + ": " + str(round((y/total)*100,2)) + "% (" + str(y) + ")"
         if y > winnervotes:
             winner = x
             winnervotes = y
-    output = output + "\n--------------------------\nWinner: " + winner + "\n--------------------------"
+    output += "\n--------------------------\nWinner: " + winner + "\n--------------------------"
     return output
 
 # output of results to the terminal
